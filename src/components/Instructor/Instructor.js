f82fce1dd8fc372b2c78152c57a728d6ff0cc802
Amareya Allen-Dabney <a.allendabney@gmail.com>
2019-05-22 15:11:19 -0500

    set up add instructor form

--- src/components/Instructor/Instructor.js
+++ src/components/Instructor/Instructor.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
-import { Icon, Label, Menu, Table } from 'semantic-ui-react'
+import { Icon, Table, Button } from 'semantic-ui-react'
 import './Instructor.css'
 
 class Instructor extends Component {
-  addInstructor = (event) => {
-    console.log('hello');
-    console.log('event:', event);
+  addInstructor = () => {
+    console.log('Add new instructor button clicked')
     this.props.history.push('/add-new-instructor')
+  }
+
+  editInstructor = () => {
+    console.log('Edit instructor button clicked')
+    //this.props.history.push('/add-new-instructor')
   }
 
   componentDidMount(){
@@ -18,9 +22,9 @@ class Instructor extends Component {
   render() {
     return (
       <div>
-        <h1>Create Instructor Table!!!!</h1>
-        <button type="button" onClick={this.addInstructor}>Add New Instructor</button>
-        <pre>{JSON.stringify(this.props.reduxState.instructor)}</pre>
+        <h1>All Instructors</h1>
+        <Button onClick={this.addInstructor}>Add New Instructor</Button>
+        {/* <pre>{JSON.stringify(this.props.reduxState.instructor)}</pre> */}
         <Table celled>
           <Table.Header>
             <Table.Row>
@@ -32,12 +36,12 @@ class Instructor extends Component {
 
           <Table.Body>
             {this.props.reduxState.instructor.map(instructor =>
-              <Table.Row>
+              <Table.Row key={instructor.id}>
                 <Table.Cell>{instructor.instructor_name}</Table.Cell>
                 <Table.Cell>{instructor.instructor_email}</Table.Cell>
                 <Table.Cell>
                   <span className="icon">{instructor.phone_number}</span>
-                  <Icon name="edit"></Icon>
+                  <Icon name="edit" onClick={this.editInstructor}></Icon>
                 </Table.Cell>
               </Table.Row>
               )}
