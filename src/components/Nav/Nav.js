b9d68058f19ffaed2429f9ed344a17dd3131580c
Will Howes <willmhowes@gmail.com>
2019-05-22 12:27:22 -0500

    make navbar links functional

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,90 +1,100 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { withRouter, Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import LogOutButton from '../LogOutButton/LogOutButton';
+import { Menu } from 'semantic-ui-react';
 import './Nav.css';
-import { Menu } from 'semantic-ui-react'
 
 class Nav extends Component {
-  state = {}
+  state = {
+    activeItem: null,
+  }
 
-  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
+  handleItemClick = (event) => {
+    console.log('in handleItemClick');
+    const value = event.target.getAttribute('value');
+
+    if (value === 'view-instructors') {
+      console.log('view-instructors button activated');
+      this.props.history.push('/view-instructors');
+      this.setState({ activeItem: value });
+    } else if (value === 'session-history') {
+      console.log('session-history button activated');
+      this.props.history.push('/session-history');
+      this.setState({ activeItem: value });
+    } else if (value === 'current-session') {
+      console.log('current-session button activated');
+      this.props.history.push('/current-session');
+      this.setState({ activeItem: value });
+    } else if (value === 'future-session') {
+      console.log('future-session button activated');
+      this.props.history.push('/future-session');
+      this.setState({ activeItem: value });
+    } else if (value === 'add-new-class') {
+      console.log('add-new-class button activated');
+    } else if (value === 'logout') {
+      this.props.dispatch({ type: 'LOGOUT' });
+    }
+  };
 
   render() {
-  const { activeItem } = this.state
+    const { activeItem } = this.state;
 
-  return (
-  <div className="nav">
-    <Link to="/home">
-    <h2 className="nav-title">MinnePlan</h2>
-    </Link>
-    <div className="nav-right">
-      {/* Show the link to the info page and the logout button if the user is logged in */}
-      {this.props.user.id && (
-        <>
-        <Menu>
-        <Menu.Item
-          name='instructor'
-          active={activeItem === 'instructor'}
-          onClick={this.handleItemClick}
-          to="/instructor"
-          className="nav-item"
-        >
-          View Instructors
-        </Menu.Item>
+    return (
+      <div className="nav">
+        <Link to="/future-session">
+          <h2 className="nav-title">MinnePlan</h2>
+        </Link>
 
-        <Menu.Item
-          name='history'
-          active={activeItem === 'history'}
-          onClick={this.handleItemClick}
-          to="/history"
-          className="nav-item"
-        >
-            History
-        </Menu.Item>
+        <div className="nav-right">
+          <Menu>
+            <Menu.Item
+              value="view-instructors"
+              onClick={this.handleItemClick}
+              active={activeItem === 'view-instructors'}
+            >
+              View Instructors
+            </Menu.Item>
+            <Menu.Item
+              value="session-history"
+              onClick={this.handleItemClick}
+              active={activeItem === 'session-history'}
+            >
+              History
+            </Menu.Item>
+            <Menu.Item
+              value="current-session"
+              onClick={this.handleItemClick}
+              active={activeItem === 'current-session'}
+            >
+              Current Session
+            </Menu.Item>
+            <Menu.Item
+              value="future-session"
+              onClick={this.handleItemClick}
+              active={activeItem === 'future-session'}
+            >
+              Future Session
+            </Menu.Item>
+            <Menu.Item
+              value="add-new-class"
+              onClick={this.handleItemClick}
+              active={activeItem === 'add-new-class'}
+            >
+              Add New Class
+            </Menu.Item>
 
-        <Menu.Item
-          name='current-session'
-          active={activeItem === 'current-session'}
-          onClick={this.handleItemClick}
-          to="/current-session"
-          className="nav-item"
-        >
-          Current Session
-        </Menu.Item>
-
-        <Menu.Item
-          name='future-session'
-          active={activeItem === 'future-session'}
-          onClick={this.handleItemClick}
-          to="/home"
-          className="nav-item"
-        >
-          Future Session
-        </Menu.Item>
-        <Menu.Item
-          name='add-new-class'
-          active={activeItem === 'add-new-class'}
-          onClick={this.handleItemClick}
-          to="/add-new-class"
-          className="nav-item"
-        >
-          Add New Class
-        </Menu.Item>
-        <Menu.Item
-          name='logout'
-          active={activeItem === 'logout'}
-          onClick={this.handleItemClick}
-          className="nav-item"
-        >
-          <LogOutButton className="nav-link" />
-        </Menu.Item>
-      </Menu>
-        </>
-      )}
-    </div>
-  </div>
-  );
+            <Menu.Menu position="right">
+              <Menu.Item
+                value="logout"
+                onClick={this.handleItemClick}
+              >
+                Logout
+              </Menu.Item>
+            </Menu.Menu>
+          </Menu>
+        </div>
+      </div>
+    );
   }
 }
 
@@ -97,4 +107,4 @@ const mapStateToProps = state => ({
   user: state.user,
 });
 
-export default connect(mapStateToProps)(Nav);
+export default connect(mapStateToProps)(withRouter(Nav));
