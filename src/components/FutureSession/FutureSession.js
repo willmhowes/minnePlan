8ea951bcdf7f9904d3e9a076090989ea5f3966cc
Will Howes <willmhowes@gmail.com>
2019-05-22 16:12:35 -0500

    create basic table for FutureSessionTableRow

--- src/components/FutureSession/FutureSession.js
+++ src/components/FutureSession/FutureSession.js
@@ -1,12 +1,61 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
+import {
+  Table, Menu, Icon,
+} from 'semantic-ui-react';
+import FutureSessionTableRow from '../FutureSessionTableRow/FutureSessionTableRow';
+import './FutureSession.css';
+
+const pretendData = [1, 2, 3];
 
 class FutureSession extends Component {
   render() {
     return (
-      <div>
-        <h1>Create Future Session Table!!!!</h1>
+      <div className="FutureSession-table-container">
+        <br />
+        <Table celled>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Select</Table.HeaderCell>
+              <Table.HeaderCell>Instructor Name</Table.HeaderCell>
+              <Table.HeaderCell>Instructor Email</Table.HeaderCell>
+              <Table.HeaderCell>Course Name</Table.HeaderCell>
+              <Table.HeaderCell>Start Date</Table.HeaderCell>
+              <Table.HeaderCell>End Date</Table.HeaderCell>
+              <Table.HeaderCell>Day of the Week</Table.HeaderCell>
+              <Table.HeaderCell>Time of Day</Table.HeaderCell>
+              <Table.HeaderCell>Num. of instances</Table.HeaderCell>
+              <Table.HeaderCell>Course Rate</Table.HeaderCell>
+              <Table.HeaderCell>Course Status</Table.HeaderCell>
+              <Table.HeaderCell>Course Description</Table.HeaderCell>
+              <Table.HeaderCell>Edit Course</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+
+          <Table.Body>
+            {pretendData.map(() => <FutureSessionTableRow />)}
+          </Table.Body>
+
+          <Table.Footer>
+            <Table.Row>
+              <Table.HeaderCell colSpan="13">
+                <Menu floated="right" pagination>
+                  <Menu.Item as="a" icon>
+                    <Icon name="chevron left" />
+                  </Menu.Item>
+                  <Menu.Item as="a">1</Menu.Item>
+                  <Menu.Item as="a">2</Menu.Item>
+                  <Menu.Item as="a">3</Menu.Item>
+                  <Menu.Item as="a">4</Menu.Item>
+                  <Menu.Item as="a" icon>
+                    <Icon name="chevron right" />
+                  </Menu.Item>
+                </Menu>
+              </Table.HeaderCell>
+            </Table.Row>
+          </Table.Footer>
+        </Table>
       </div>
     );
   }
