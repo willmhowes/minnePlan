ff46af80a28d060e0e0a0bcb7fea3761993d4c1f
Amareya Allen-Dabney <a.allendabney@gmail.com>
2019-05-23 11:24:20 -0500

    server set-up for instructor-schedule is set-up

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const passport = require('./strategies/user.strategy');
 // Route includes
 const userRouter = require('./routes/user.router');
 const instructorRouter = require('./routes/instructor.router');
+const instructorScheduleRouter = require('./routes/instructorSchedule.router');
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -25,6 +26,7 @@ app.use(passport.session());
 /* Routes */
 app.use('/api/user', userRouter);
 app.use('/api/instructor', instructorRouter);
+app.use('/instructorSchedule', instructorScheduleRouter);
 
 // Serve static files
 app.use(express.static('build'));
