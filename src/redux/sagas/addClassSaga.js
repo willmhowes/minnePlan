d6ecbfb32b79f58de9f51b522220b2101ce5d339
Brian Zilka <Bzilks@gmail.com>
2019-05-24 18:05:42 -0500

    POST route working and sending to the database

--- src/redux/sagas/addClassSaga.js
+++ src/redux/sagas/addClassSaga.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 import { takeLatest } from 'redux-saga/effects';
 
 function* addClassSaga(action) {
-  console.log('Hit the collection', action);
+  console.log('Hit the addClassSaga', action);
   try {
-    // Attempt to get collection
-    const response = yield axios.post('/api/class', action.payload);
+    // Attempt to get class
+    const response = yield axios.post('/api/add-new-class', action.payload);
     console.log(response);
   } catch (error) {
     console.log('Couldn\'t add class', error);
