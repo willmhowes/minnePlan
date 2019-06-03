02b73e5d50d5b5b4c475d92c56fc73c6803a020d
Jarvis Yang <jarvisgyang@gmail.com>
2019-06-03 09:22:41 -0500

    updated instructor email message

--- server/modules/emailHandler.js
+++ server/modules/emailHandler.js
@@ -14,8 +14,13 @@ function sendEmail(email, res) {
   const mailOptions = {
     from: 'noreplymplscommed@gmail.com', // sender address
     to: email, // list of receivers
-    subject: 'IT WORKS!', // Subject line
-    html: `<p>${email}, hello friend</p>`, // plain text body
+    subject: 'Please review your schedule!', // Subject line
+    html: `<p>Hello instructor!</p>
+      <p>Your proposed schedule for the upcoming session of the Minneapolis Public School District's community education program is now available for review.</p>
+      <p>Please click this <a href="http://localhost:3000/instructor_login" target="_blank" rel="noopener noreferrer">link</a> to review your schedule, or copy and paste the link below into the URL bar of your browser.</p>
+      <p><a href="http://localhost:3000/instructor_login" target="_blank" rel="noopener noreferrer">http://localhost:3000/instructor_login</a></p>
+      <p>For any additional questions, please reach out to Eliana Power directly. This email address is not monitored.</p>
+      <p>Thank you!</p>`, // plain text body
   };
 
   transporter.sendMail(mailOptions, (err, info) => {
