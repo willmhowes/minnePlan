229e544bba767492544b16263527abe52e402216
Amareya Allen-Dabney <a.allendabney@gmail.com>
2019-06-05 15:40:40 -0500

    updated sessions.router.js

--- server/routes/session.router.js
+++ server/routes/session.router.js
@@ -4,9 +4,7 @@ const { rejectUnauthenticated } = require('../modules/authentication-middleware'
 
 const router = express.Router();
 
-/**
- * GET route template
- */
+// getting all sessions
 router.get('/', rejectUnauthenticated, (req, res) => {
   const sessionQuery = 'SELECT "sessions"."id", "seasons"."season", "years"."years" FROM "sessions" JOIN "seasons" ON "sessions"."season" = "seasons"."id" JOIN "years" ON "sessions"."year" = "years"."id";';
   pool.query(sessionQuery)
@@ -16,6 +14,7 @@ router.get('/', rejectUnauthenticated, (req, res) => {
     });
 });
 
+// getting all years
 router.get('/year', rejectUnauthenticated, (req, res) => {
   const yearQuery = 'SELECT * FROM "years" ORDER BY "years" ASC;';
   pool.query(yearQuery)
@@ -25,6 +24,7 @@ router.get('/year', rejectUnauthenticated, (req, res) => {
     });
 });
 
+// getting all years
 router.get('/season', rejectUnauthenticated, (req, res) => {
   const yearQuery = 'SELECT * FROM "seasons";';
   pool.query(yearQuery)
@@ -34,21 +34,29 @@ router.get('/season', rejectUnauthenticated, (req, res) => {
     });
 });
 
+// update sessions when starting a new session
 router.put('/', rejectUnauthenticated, async (req, res) => {
   const client = await pool.connect();
   const updateCurrentSession = 'UPDATE "sessions" SET "session_status" = \'archived\' WHERE "session_status" = \'current\';';
   const updateFutureSession = 'UPDATE "sessions" SET "session_status" = \'current\' WHERE "session_status" = \'planning\';';
   const newSession = 'INSERT INTO "sessions" ("coordinator_ref", "season", "year", "session_status") VALUES (1, $1, $2, \'planning\');';
   try {
+    // begins transaction
     await client.query('BEGIN');
+    // updates all current sessions to archived sessions
     await client.query(updateCurrentSession);
+    // updates all planning sessions to current sessions
     await client.query(updateFutureSession);
+    // creates a new session wit req.body
     await client.query(newSession, [req.body.season, req.body.year]);
+    // proceeds with all sql if they all go through
     await client.query('COMMIT');
   } catch (error) {
+    // aborts all sql queries if 1 fails
     await client.query('ROLLBACK');
     res.sendStatus(500);
   } finally {
+    // releases transaction
     client.release();
     res.sendStatus(200);
   }
